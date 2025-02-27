d00f7aaff2e2a2f385b9cd0f13a441f8aed5140d
Gavin Gammoh <gammohgb@mail.uc.edu>
2025-02-27 14:24:24 -0500

    CLEANUP

--- examples/test_autoclean.sh
+++
@@ -1,6 +0,0 @@
-autoclean.sh \
-    -DataPath "/mnt/srv2/robots/aud_assr/input/allego_10__uid0122-15-58-45_data.set" \
-    -Task "MouseXdatAssr" \
-    -ConfigPath "/mnt/srv2/robots/aud_assr/configs/autoclean_config.yaml" \
-    -WorkDir "/mnt/srv2/eeg_dependencies/autoclean_pipeline/" \
-    -OutputPath "/mnt/srv2/robots/aud_assr/output"
